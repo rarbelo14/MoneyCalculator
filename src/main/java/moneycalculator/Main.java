24fc1b7b6366823d6204bc3a0c26314ab408eb9a
Ricardo <Ricardo@DESKTOP-LPK2S35>
2020-10-24 19:10:46 +0100

    (WIK) Model and User Inteface

---
+++ src/main/java/moneycalculator/Main.java
@@ -0,0 +1,13 @@
+package moneycalculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        
+        // I ended up watching 7:00 of the video so far.
+        
+    }
+
+}
